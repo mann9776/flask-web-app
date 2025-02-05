e31e6d192b287b234ba8c2cf7ad0a6c89c93387d
uday5252 <73519118+uday5252@users.noreply.github.com>
2025-02-05 14:09:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+
+# Enable Prometheus metrics
+metrics = PrometheusMetrics(app)
 
 @app.route("/wish")
 def pleaseWish():
